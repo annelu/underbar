33190b1a370ffd7da66464db756426c321e959b7
annelu <annelu@notalion.com>
2013-09-09 13:58:18 -0700

    Added _.every

--- src/underbar.js
+++ src/underbar.js
@@ -181,12 +181,22 @@ var _ = { };
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    iterator = iterator || function(item) {
+      return item;
+    };
+    var result =  _.reduce(collection, function(isTrue, item){
+      return !!iterator(item) && isTrue;
+    }, true);
+    return result;
   };
+
+
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    _.every(collection, !iterator);
   };
 
 
